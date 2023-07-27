41cf88b0b3d39ad2826504122a9dd1aa07593349
Amauri Rodrigues <amauriaureo@gmail.com>
2023-07-27 06:40:37 -0300

    ModelForm

---
+++ reserva/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from reserva.models import Reserva
+
+
+class ReservaForm(forms.ModelForm):
+
+    class Meta:
+        model = Reserva
+        fields = [
+            'nome', 'email', 'nome_pet', 'data', 'turno',
+            'tamanho', 'observacoes'
+        ]
