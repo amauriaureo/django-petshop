e209eb354e2a3a67d31045acc5d0ff5b92e2d36f
Amauri Rodrigues <amauriaureo@gmail.com>
2023-08-25 06:46:34 -0300

    AgendamentoMOdelSerializer

--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -1,5 +1,9 @@
 from rest_framework.serializers import ModelSerializer
 
+from reserva.models import Reserva
+
 
 class AgendamentoModelSerializer(ModelSerializer):
-    pass
+    class Meta:
+        model = Reserva
+        fields = '__all__'
